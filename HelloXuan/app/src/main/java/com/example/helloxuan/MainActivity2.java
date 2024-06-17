aafd9cb38cdad16ad74c98554665dd4067227261
yuxuan <1790618164@qq.com>
2024-06-17 19:03:16 +0800

    first commit

---
+++ HelloXuan/app/src/main/java/com/example/helloxuan/MainActivity2.java
@@ -0,0 +1,15 @@
+package com.example.helloxuan;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity2 extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
